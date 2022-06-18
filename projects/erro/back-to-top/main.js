bed73db5d71e9bac003a5af343ec5921b5ebf318
arshia788 <104620759+arshia788@users.noreply.github.com>
2022-06-18 23:26:59 +0430

    Update main.js

--- projects/erro/back-to-top/main.js
+++ projects/erro/back-to-top/main.js
@@ -1,8 +1,7 @@
 const toTop= document.querySelector('.to-top')
 
 
-// omadam ertefah on item ro az bala gereftam va goftam agar az in bishatr shod class ro bedeh
-// dg add nadam behesh
+
 
 window.addEventListener('scroll', function (){
        // agar safhe az 200px bishtar bashe miad class mideh be to-top
