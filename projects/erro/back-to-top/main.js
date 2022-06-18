7bbc96e01cd77c6c2e6ec349c4148deef388abcd
arshia788 <104620759+arshia788@users.noreply.github.com>
2022-06-18 23:28:02 +0430

    Update main.js

--- projects/erro/back-to-top/main.js
+++ projects/erro/back-to-top/main.js
@@ -5,6 +5,7 @@ const toTop= document.querySelector('.to-top')
 
 window.addEventListener('scroll', function (){
        // agar safhe az 200px bishtar bashe miad class mideh be to-top
+       // baray in toggle nadadi chon hey miad o mireh valy to mikhy in bashe va agar nashod shart bereh
        if(window.pageYOffset > 200){
               toTop.classList.add('active')
        }
